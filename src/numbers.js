b35bfa2edbe1f38609492465017f16162551a2b5
Ash Askin <ashaskincodes@gmail.com>
2022-05-23 10:54:31 +0100

    started

--- src/numbers.js
+++ src/numbers.js
@@ -1,17 +1,17 @@
-function add (a, b) {
-  // your code here
+const add = (a, b) => {
+  return a + b;
 }
 
-function subtract (a, b) {
-  // your code here
+const subtract = (a, b) => {
+  return a - b;
 }
 
-function multiply (a, b) {
-  // your code here
+const multiply = (a, b) => {
+  return a * b;
 }
 
-function divide (a, b) {
-  // your code here
+const divide = (a, b) => {
+  return a / b;
 }
 
 function power (a, b) {
